c60f751786c3873aca2f8095092df95c9f94db65
Ryan Bradley <rbradley@redhat.com>
2012-04-06 16:51:16 -0400

    Add domain object Member

---
+++ jboss-as-kitchen-sink/src/main/java/org/jboss/as/quickstarts/kitchensink/model/Member.java
@@ -0,0 +1,60 @@
+package org.jboss.as.quickstarts.kitchensink.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class Member implements Serializable {
+   /** Default value included to remove warning. Remove or modify at will. **/
+   private static final long serialVersionUID = 1L;
+
+   @Id
+   @GeneratedValue
+   private Long id;
+
+   private String name;
+
+   private String email;
+
+   @Column(name = "phone_number")
+   private String phoneNumber;
+
+   public Long getId() {
+      return id;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   public String getPhoneNumber() {
+      return phoneNumber;
+   }
+
+   public void setPhoneNumber(String phoneNumber) {
+      this.phoneNumber = phoneNumber;
+   }
+}
