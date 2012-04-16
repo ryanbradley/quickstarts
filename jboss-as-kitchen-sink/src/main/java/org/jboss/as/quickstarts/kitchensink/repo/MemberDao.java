205f60131f8d108e4e05157da89859a785c01424
Ryan Bradley <rbradley@redhat.com>
2012-04-16 17:28:36 -0400

    Begin work on MemberDao and its implementation

--- jboss-as-kitchen-sink/src/main/java/org/jboss/as/quickstarts/kitchensink/repo/MemberDao.java
+++ jboss-as-kitchen-sink/src/main/java/org/jboss/as/quickstarts/kitchensink/repo/MemberDao.java
@@ -1,6 +1,9 @@
 package org.jboss.as.quickstarts.kitchensink.repo;
 
+import org.jboss.as.quickstarts.kitchensink.model.Member;
+
 public interface MemberDao
 {
+    public void register(Member member);
 
 }
--- jboss-as-kitchen-sink/src/main/java/org/jboss/as/quickstarts/kitchensink/repo/MemberDaoImpl.java
+++ jboss-as-kitchen-sink/src/main/java/org/jboss/as/quickstarts/kitchensink/repo/MemberDaoImpl.java
@@ -1,5 +1,9 @@
 package org.jboss.as.quickstarts.kitchensink.repo;
 
+import javax.persistence.EntityManager;
+
+import org.jboss.as.quickstarts.kitchensink.model.Member;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -7,5 +11,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class MemberDaoImpl implements MemberDao
 {
+    @Autowired
+    private EntityManager entityManager;
+
+    public void register(Member member)
+    {
+        entityManager.persist(member);
+        return;
+    }
 
 }
