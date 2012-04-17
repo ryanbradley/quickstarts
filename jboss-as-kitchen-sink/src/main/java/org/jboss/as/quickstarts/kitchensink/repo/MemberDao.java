90d7edf878337c62129b9afdabd6398ca4094c13
Ryan Bradley <rbradley@redhat.com>
2012-04-17 10:18:12 -0400

    Create a MemberDao interface and implementation for database access.

--- jboss-as-kitchen-sink/src/main/java/org/jboss/as/quickstarts/kitchensink/repo/MemberDao.java
+++ jboss-as-kitchen-sink/src/main/java/org/jboss/as/quickstarts/kitchensink/repo/MemberDao.java
@@ -1,9 +1,16 @@
 package org.jboss.as.quickstarts.kitchensink.repo;
 
+import java.util.List;
+
 import org.jboss.as.quickstarts.kitchensink.model.Member;
 
 public interface MemberDao
 {
+    public List<Member> getMembers();
+
+    public List<Member> getMembersOrderedByName();
+
     public void register(Member member);
 
+    public Member getMember(Long id);
 }
--- jboss-as-kitchen-sink/src/main/java/org/jboss/as/quickstarts/kitchensink/repo/MemberDaoImpl.java
+++ jboss-as-kitchen-sink/src/main/java/org/jboss/as/quickstarts/kitchensink/repo/MemberDaoImpl.java
@@ -1,6 +1,12 @@
 package org.jboss.as.quickstarts.kitchensink.repo;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import org.jboss.as.quickstarts.kitchensink.model.Member;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +20,36 @@ public class MemberDaoImpl implements MemberDao
     @Autowired
     private EntityManager entityManager;
 
+    @SuppressWarnings("unchecked")
+    public List<Member> getMembers()
+    {
+        Query query = entityManager.createQuery("select m from Member m");
+        return query.getResultList();
+    }
+
+    public List<Member> getMembersOrderedByName()
+    {
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Member> criteria = builder.createQuery(Member.class);
+        Root<Member> member = criteria.from(Member.class);
+
+        /*
+         * Swap criteria statements if you would like to try out type-safe criteria queries, a new
+         * feature in JPA 2.0 criteria.select(member).orderBy(cb.asc(member.get(Member_.name)));
+         */
+
+        criteria.select(member).orderBy(builder.asc(member.get("name")));
+        return entityManager.createQuery(criteria).getResultList();
+    }
+
     public void register(Member member)
     {
         entityManager.persist(member);
         return;
     }
 
+    public Member getMember(Long id)
+    {
+        return entityManager.find(Member.class, id);
+    }
 }
